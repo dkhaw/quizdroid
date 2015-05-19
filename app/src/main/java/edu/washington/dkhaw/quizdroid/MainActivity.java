8768f8e1eddf25aef02bd65e1d317487807554a8
Drew <drewkhaw@gmail.com>
2015-05-19 15:54:09 -0700

    added a file fetch to receive downloaded JSON

--- app/src/main/java/edu/washington/dkhaw/quizdroid/MainActivity.java
+++ app/src/main/java/edu/washington/dkhaw/quizdroid/MainActivity.java
@@ -26,6 +26,7 @@ import android.widget.Toast;
 
 import org.json.JSONException;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -53,16 +54,32 @@ public class MainActivity extends ActionBarActivity {
         //filter.addAction(DownloadManager.ACTION_DOWNLOAD_COMPLETE); // Add more filters here that you want the receiver to listen to
         //registerReceiver(receiver, filter);
 
+        File myFile = new File(getFilesDir().getAbsolutePath(), "/data.json");
 
-        // Fetch data.json in assets/ folder
-        AssetManager assetManager = getAssets();
-        try {
-            InputStream input = assetManager.open("questions.json");
-            repo = new JSONRepository(input);
-            // use hardcoded data as a backup
-        } catch (IOException | JSONException e) {
-            repo = new HardcodedRepository();
+        // check if data.json file exists in files directory
+        if (myFile.exists()) {
+            Log.i("MyApp", "data.json DOES exist");
+
+            try {
+                FileInputStream fis = openFileInput("data.json");
+                repo = new JSONRepository(fis);
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            // Fetch data.json in assets/ folder
+            AssetManager assetManager = getAssets();
+            try {
+                InputStream input = assetManager.open("questions.json");
+                repo = new JSONRepository(input);
+                // use hardcoded data as a backup
+            } catch (IOException | JSONException e) {
+                repo = new HardcodedRepository();
+            }
         }
+
+        // DownloadService.changeAlarm(this, true);
 
         String[] topics = repo.getAllTopics();
         ListView listView = (ListView) findViewById(R.id.listView);
@@ -128,65 +145,7 @@ public class MainActivity extends ActionBarActivity {
     private final BroadcastReceiver receiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            dm = (DownloadManager)getSystemService(DOWNLOAD_SERVICE);
-
-            if (DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(action)) {
-                long downloadID = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, 0);
-
-                if (downloadID != 0) {
-
-                    // Check status
-                    DownloadManager.Query query = new DownloadManager.Query();
-                    query.setFilterById(downloadID);
-                    Cursor c = dm.query(query);
-                    if(c.moveToFirst()) {
-                        int status = c.getInt(c.getColumnIndex(DownloadManager.COLUMN_STATUS));
-                        Log.d("DM Sample","Status Check: "+status);
-                        switch(status) {
-                            case DownloadManager.STATUS_PAUSED:
-                            case DownloadManager.STATUS_PENDING:
-                            case DownloadManager.STATUS_RUNNING:
-                                break;
-                            case DownloadManager.STATUS_SUCCESSFUL:
-                                // The download-complete message said the download was "successfu" then run this code
-                                ParcelFileDescriptor file;
-                                StringBuffer strContent = new StringBuffer("");
-
-                                try {
-                                    // Get file from Download Manager (which is a system service as explained in the onCreate)
-                                    file = dm.openDownloadedFile(downloadID);
-                                    FileInputStream fis = new FileInputStream(file.getFileDescriptor());
-
-                                    // YOUR CODE HERE [convert file to String here]
-
-
-
-                                    // YOUR CODE HERE [write string to data/data.json]
-                                    //      [hint, i wrote a writeFile method in MyApp... figure out how to call that from inside this Activity]
-
-                                    // convert your json to a string and echo it out here to show that you did download it
-
-
-
-                                    /*
-                                    String jsonString = ....myjson...to string().... chipotle burritos.... blah
-                                    Log.i("MyApp - Here is the json we download:", jsonString);
-                                    */
-
-                                } catch (FileNotFoundException e) {
-                                    e.printStackTrace();
-                                } catch (IOException e) {
-                                    e.printStackTrace();
-                                }
-                                break;
-                            case DownloadManager.STATUS_FAILED:
-                                // YOUR CODE HERE! Your download has failed! Now what do you want it to do? Retry? Quit application? up to you!
-                                break;
-                        }
-                    }
-                }
-            }
+            // start a DownloadManager
         }
     };
 }
