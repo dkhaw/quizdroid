32be5ff532e141b4f84c38e8aa720a3039df45df
Drew <drewkhaw@gmail.com>
2015-05-22 23:58:34 -0700

    implemented download service

--- app/src/main/java/edu/washington/dkhaw/quizdroid/DownloadService.java
+++ app/src/main/java/edu/washington/dkhaw/quizdroid/DownloadService.java
@@ -7,26 +7,36 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.util.Log;
 
 public class DownloadService extends IntentService {
-    private int interval= MainActivity.interval;
-    private String url = MainActivity.url;
     private DownloadManager dm;
-    private long enqueue;
     public static final int ALARM = 123;
+    private static int interval= MainActivity.interval;
+    private String url = MainActivity.url;
 
     public DownloadService() {
         super("DownloadService");
     }
 
     @Override
-    protected void onHandleIntent(Intent workIntent) {
-        dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
-        DownloadManager.Request req = new DownloadManager.Request(Uri.parse(url));
-        enqueue = dm.enqueue(req);
+    public void onCreate() { super.onCreate(); }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
     }
 
-    public void changeAlarm(Context context, boolean on) {
+    @Override
+    protected void onHandleIntent(Intent workIntent) {
+
+        Log.i("xxx", "onHandleIntent fired");
+        dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+        DownloadManager.Request req = new DownloadManager.Request(Uri.parse(url));
+        dm.enqueue(req);
+    }
+
+    public static void changeAlarm(Context context, boolean on) {
         Intent alarmReceiverIntent = new Intent(context, AlarmReceiver.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(context, ALARM, alarmReceiverIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         AlarmManager manager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
--- app/src/main/java/edu/washington/dkhaw/quizdroid/MainActivity.java
+++ app/src/main/java/edu/washington/dkhaw/quizdroid/MainActivity.java
@@ -1,18 +1,23 @@
 package edu.washington.dkhaw.quizdroid;
 
 import android.app.AlarmManager;
+import android.app.AlertDialog;
 import android.app.DownloadManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.res.AssetManager;
 import android.database.Cursor;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.ParcelFileDescriptor;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -29,6 +34,7 @@ import org.json.JSONException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -40,7 +46,6 @@ public class MainActivity extends ActionBarActivity {
     public static int interval = -1;
     private static final int PREFERENCES_RESULT = 1;
     private PendingIntent pendingIntent;
-    private DownloadManager dm;
     private QuizApp.TopicRepository repo;
 
     @Override
@@ -50,25 +55,22 @@ public class MainActivity extends ActionBarActivity {
         Intent alarmIntent = new Intent(MainActivity.this, AlarmReceiver.class);
         pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, alarmIntent, 0);
 
-        //IntentFilter filter = new IntentFilter();
-        //filter.addAction(DownloadManager.ACTION_DOWNLOAD_COMPLETE); // Add more filters here that you want the receiver to listen to
-        //registerReceiver(receiver, filter);
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(DownloadManager.ACTION_DOWNLOAD_COMPLETE);
+        registerReceiver(receiver, filter);
 
-        File myFile = new File(getFilesDir().getAbsolutePath(), "/data.json");
+        File myFile = new File(getFilesDir().getAbsolutePath(), "/questions.json");
 
         // check if data.json file exists in files directory
         if (myFile.exists()) {
-            Log.i("MyApp", "data.json DOES exist");
-
             try {
-                FileInputStream fis = openFileInput("data.json");
+                FileInputStream fis = openFileInput("questions.json");
                 repo = new JSONRepository(fis);
             } catch (IOException | JSONException e) {
                 e.printStackTrace();
             }
-
+        // Fetch data.json in assets/ folder
         } else {
-            // Fetch data.json in assets/ folder
             AssetManager assetManager = getAssets();
             try {
                 InputStream input = assetManager.open("questions.json");
@@ -79,7 +81,7 @@ public class MainActivity extends ActionBarActivity {
             }
         }
 
-        // DownloadService.changeAlarm(this, true);
+        //DownloadService.changeAlarm(this, true);
 
         String[] topics = repo.getAllTopics();
         ListView listView = (ListView) findViewById(R.id.listView);
@@ -125,14 +127,44 @@ public class MainActivity extends ActionBarActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == PREFERENCES_RESULT) {
-            SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-            url = sharedPrefs.getString("prefDataUrl", "NODATAURL");
-            interval = Integer.valueOf(sharedPrefs.getString("prefDownloadInterval", "NODOWNLOADINTERVAL")) * 60000;
-            Toast.makeText(this, url, Toast.LENGTH_SHORT).show(); // to be removed later
-            /*AlarmManager manager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-            manager.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), interval, pendingIntent);*/
+        if (airplaneMode(this)) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("You are currently in Airplane Mode. You must turn this off before a download can occur.")
+                    .setTitle("Airplane Mode");
+            builder.setPositiveButton("Go to settings", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    startActivityForResult(new Intent(android.provider.Settings.ACTION_SETTINGS), 0);
+                }
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+
+                }
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        } else if (!online()) {
+            Toast.makeText(this, "Error downloading: You are currently offline", Toast.LENGTH_LONG).show();
+        } else {
+            if (requestCode == PREFERENCES_RESULT) {
+                SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+                url = sharedPrefs.getString("prefDataUrl", "NODATAURL");
+                interval = Integer.valueOf(sharedPrefs.getString("prefDownloadInterval", "NODOWNLOADINTERVAL")) * 60000;
+                AlarmManager manager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+                manager.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), interval, pendingIntent);
+            }
         }
+    }
+
+    private static boolean airplaneMode(Context context) {
+        return Settings.System.getInt(context.getContentResolver(), Settings.System.AIRPLANE_MODE_ON, 0) != 0;
+    }
+
+    public boolean online() {
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
     }
 
     @Override
@@ -145,7 +177,74 @@ public class MainActivity extends ActionBarActivity {
     private final BroadcastReceiver receiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            // start a DownloadManager
+            String action = intent.getAction();
+            DownloadManager dm = (DownloadManager)getSystemService(DOWNLOAD_SERVICE);
+
+            if (DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(action)) {
+                long downloadID = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, 0);
+
+                // if the downloadID exists
+                if (downloadID != 0) {
+                    // Check status
+                    DownloadManager.Query query = new DownloadManager.Query();
+                    query.setFilterById(downloadID);
+                    Cursor c = dm.query(query);
+                    if(c.moveToFirst()) {
+                        int status = c.getInt(c.getColumnIndex(DownloadManager.COLUMN_STATUS));
+                        switch(status) {
+                            case DownloadManager.STATUS_PAUSED:
+                            case DownloadManager.STATUS_PENDING:
+                            case DownloadManager.STATUS_RUNNING:
+                                break;
+                            case DownloadManager.STATUS_SUCCESSFUL:
+                                ParcelFileDescriptor file;
+                                StringBuffer sb = new StringBuffer("");
+                                int ch;
+                                try {
+                                    file = dm.openDownloadedFile(downloadID);
+                                    FileInputStream fis = new FileInputStream(file.getFileDescriptor());
+                                    while((ch = fis.read()) != -1) {
+                                        sb.append((char)ch);
+                                    }
+                                    String data = sb.toString();
+                                    try {
+                                        File newFile = new File(getFilesDir().getAbsolutePath(), "questions.json");
+                                        FileOutputStream fos = new FileOutputStream(newFile);
+                                        fos.write(data.getBytes());
+                                        fos.close();
+                                    }
+                                    catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                    Log.i("MainActivity","json downloaded:" + data);
+
+                                } catch (FileNotFoundException e) {
+                                    e.printStackTrace();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                                break;
+                            case DownloadManager.STATUS_FAILED:
+                                AlertDialog.Builder builder = new AlertDialog.Builder(getApplication());
+                                builder.setMessage("Download failed. Would you like to double check your URL and try again?")
+                                        .setTitle("Retry");
+                                builder.setPositiveButton("Go to settings", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        Intent i = new Intent(getApplicationContext(), PreferencesActivity.class);
+                                        startActivityForResult(i, PREFERENCES_RESULT);
+                                    }
+                                });
+                                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+
+                                    }
+                                });
+                                AlertDialog dialog = builder.create();
+                                dialog.show();                                break;
+                        }
+                    }
+                }
+            }
         }
     };
 }
